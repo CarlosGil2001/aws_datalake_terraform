85548a15bf63e35a15daba0057f9621d3d01b61a
IdhamSdmc <isacd.dannmc@gmail.com>
2024-04-10 15:43:16 -0500

    Jobs ETL Silver and Gold

--- jobs/job_silverzone.py
+++ jobs/job_silverzone.py
@@ -31,14 +31,18 @@ spark = glueContext.spark_session
 job = Job(glueContext)
 job.init(args['JOB_NAME'], args)
 
-# Script generated AWS Glue Data Catalog
-AWSGlueDataCatalog = glueContext.create_dynamic_frame.from_catalog(database="db_aws_data", table_name="testbk_1651516_bronze", transformation_ctx="AWSGlueDataCatalog")
-df_spark = AWSGlueDataCatalog.toDf()
-columns_to_drop = ['remote_ratio', 'partition_0']
-df_spark1 = df_spark.drop(*columns_to_drop)
-df_spark2 = df_spark1.withColumn("experience_level",when(col("experience_level")=="SE", "Senior Level").when(col(experience_level)=="MI", "Mid Level").when(col(experience_level)=="EN", "Entry Level").otherwise("Executive Leve").alias(""))
-dynamicDF = DynamicFrame.fromDF(df_spark2, glueContext, "dynamicDF")
+# Read table from AWS Catalog
+AWSGlueDataCatalog = glueContext.create_dynamic_frame.from_catalog(database="db_aws_data", table_name="crw_bronzezone_bk_bronzezone_project1_dev_useast1", transformation_ctx="AWSGlueDataCatalog")
+# Convert Glue DynamicFrame to PySpark DataFrame
+df_spark = AWSGlueDataCatalog.toDF()
+df_spark1 = df_jobs.drop('remote_ratio').drop('partition_0')
+df_spark2 = df_spark1.withColumn("experience_level",when(col("experience_level")=="SE", "Senior Level").when(col("experience_level")=="MI", "Mid Level").when(col("experience_level")=="EN", "Entry Level").otherwise("Executive Leve"))
+df_spark3 = df_spark2.withColumn("employment_type",when(col("employment_type")=="FT", "Full Time").when(col("employment_type")=="CT", "Contract basis").when(col("employment_type")=="FL", "Freelancer").otherwise("Part Time"))
+df_spark4 = df_spark3.withColumn("company_size",when(col("company_size")=="L", "Large").when(col("company_size")=="S", "Small").otherwise("Medium"))
+df_spark5 = df_spark4.withColumnRenamed('work_year', 'Year').withColumnRenamed('experience_level', 'Experience Level').withColumnRenamed('employment_type', 'Job Type').withColumnRenamed('job_title', 'Job Description').withColumnRenamed('salary', 'Salary').withColumnRenamed('salary_currency', 'Salary Currency').withColumnRenamed('salary_in_usd', 'Salary In USD').withColumnRenamed('employee_residence', 'Employee Location').withColumnRenamed('company_location', 'Company Location').withColumnRenamed('company_size', 'Company Size')
+# Convert PySpark DataFrame to Glue DynamicFrame
+dynamicDF = DynamicFrame.fromDF(df_spark5, glueContext, "dynamicDF")
 
-AmazonS3_output = glueContext.write_dynamic_frame.from_options(frame=dynamicDF, connection_type="s3", format="glueparquet", connection_options={"path": "s3://bk-11651651-silver/orders/", "partitionKeys": []}, format_options={"compression": "snappy"}, transformation_ctx="AmazonS3_output")
+AmazonS3_output = glueContext.write_dynamic_frame.from_options(frame=dynamicDF, connection_type="s3", format="glueparquet", connection_options={"path": "s3://bk-silverzone-project1-dev-useast1/sales/", "partitionKeys": []}, format_options={"compression": "snappy"}, transformation_ctx="AmazonS3_output")
 
 job.commit()
--- jobs/job_silverzone_bk.py
+++ jobs/job_silverzone_bk.py
@@ -2,42 +2,12 @@ import sys
 from awsglue.transforms import *
 from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
+from pyspark.sql.functions import col, when
 from awsglue.context import GlueContext
 from awsglue.job import Job
 from awsglue.gluetypes import *
 from awsglue import DynamicFrame
 
-def _find_null_fields(ctx, schema, path, output, nullStringSet, nullIntegerSet, frame):
-    if isinstance(schema, StructType):
-        for field in schema:
-            new_path = path + "." if path != "" else path
-            output = _find_null_fields(ctx, field.dataType, new_path + field.name, output, nullStringSet, nullIntegerSet, frame)
-    elif isinstance(schema, ArrayType):
-        if isinstance(schema.elementType, StructType):
-            output = _find_null_fields(ctx, schema.elementType, path, output, nullStringSet, nullIntegerSet, frame)
-    elif isinstance(schema, NullType):
-        output.append(path)
-    else:
-        x, distinct_set = frame.toDF(), set()
-        for i in x.select(path).distinct().collect():
-            distinct_ = i[path.split('.')[-1]]
-            if isinstance(distinct_, list):
-                distinct_set |= set([item.strip() if isinstance(item, str) else item for item in distinct_])
-            elif isinstance(distinct_, str) :
-                distinct_set.add(distinct_.strip())
-            else:
-                distinct_set.add(distinct_)
-        if isinstance(schema, StringType):
-            if distinct_set.issubset(nullStringSet):
-                output.append(path)
-        elif isinstance(schema, IntegerType) or isinstance(schema, LongType) or isinstance(schema, DoubleType):
-            if distinct_set.issubset(nullIntegerSet):
-                output.append(path)
-    return output
-
-def drop_nulls(glueContext, frame, nullStringSet, nullIntegerSet, transformation_ctx) -> DynamicFrame:
-    nullColumns = _find_null_fields(frame.glue_ctx, frame.schema(), "", [], nullStringSet, nullIntegerSet, frame)
-    return DropFields.apply(frame=frame, paths=nullColumns, transformation_ctx=transformation_ctx)
 
 def transform_experience_level(frame):
     def _transform_experience_level(experience_level):
@@ -62,30 +32,13 @@ job = Job(glueContext)
 job.init(args['JOB_NAME'], args)
 
 # Script generated AWS Glue Data Catalog
-AWSGlueDataCatalog = glueContext.create_dynamic_frame.from_catalog(database="db_aws_data", table_name="testbk_1651516_bronze", transformation_ctx="AWSGlueDataCatalog")
+AWSGlueDataCatalog = glueContext.create_dynamic_frame.from_catalog(database="db_aws_data", table_name="crw_bronzezone_bk_bronzezone_project1_dev_useast1", transformation_ctx="AWSGlueDataCatalog")
+df_spark = AWSGlueDataCatalog.toDF()
+columns_to_drop = ['remote_ratio', 'partition_0']
+df_spark1 = df_spark.drop(*columns_to_drop)
+df_spark2 = df_spark1.withColumn("experience_level",when(col("experience_level")=="SE", "Senior Level").when(col("experience_level")=="MI", "Mid Level").when(col("experience_level")=="EN", "Entry Level").otherwise("Executive Leve").alias(""))
+dynamicDF = DynamicFrame.fromDF(df_spark2, glueContext, "dynamicDF")
 
-# Script generated Drop Fields
-DropFields = DropFields.apply(frame=AWSGlueDataCatalog, paths=["partition_0", "remote_ratio"], transformation_ctx="DropFields")
-
-# Script generated for node Drop Null Fields
-DropNullFields = drop_nulls(glueContext, frame=DropFields, nullStringSet={"", "null"}, nullIntegerSet={-1}, transformation_ctx="DropNullFields")
-
-# Script generated Transform Experience Level
-TransformExperienceLevel = transform_experience_level(DropNullFields)
-
-# Script generated Rename Field
-RenameField_work_year = RenameField.apply(frame=TransformExperienceLevel, old_name="work_year", new_name="year", transformation_ctx="RenameField_work_year")
-RenameField_experience_level = RenameField.apply(frame=RenameField_work_year, old_name="experience_level", new_name="Experience Level", transformation_ctx="RenameField_experience_level")
-RenameField_employment_type = RenameField.apply(frame=RenameField_experience_level, old_name="employment_type", new_name="Job Type", transformation_ctx="RenameField_employment_type")
-RenameField_job_title = RenameField.apply(frame=RenameField_employment_type, old_name="job_title ", new_name="Job Description", transformation_ctx="RenameField_job_title")
-RenameField_salary = RenameField.apply(frame=RenameField_job_title, old_name="salary", new_name="Salary", transformation_ctx="RenameField_salary")
-RenameField_salary_currency = RenameField.apply(frame=RenameField_salary, old_name="salary_currency", new_name="Salary Currency", transformation_ctx="RenameField_salary_currency")
-RenameField_salary_in_usd = RenameField.apply(frame=RenameField_salary_currency, old_name="salary_in_usd", new_name="Salary In USD", transformation_ctx="RenameField_salary_in_usd")
-RenameField_employee_residence = RenameField.apply(frame=RenameField_salary_in_usd, old_name="employee_residence ", new_name="Employee Location", transformation_ctx="RenameField_employee_residence")
-RenameField_company_location = RenameField.apply(frame=RenameField_employee_residence, old_name="company_location", new_name="Company Location", transformation_ctx="RenameField_company_location")
-RenameField_company_size = RenameField.apply(frame=RenameField_company_location, old_name="company_size", new_name="Company Size", transformation_ctx="RenameField_company_size")
-
-# Script generated for node Amazon S3
-AmazonS3_output = glueContext.write_dynamic_frame.from_options(frame=RenameField_company_size, connection_type="s3", format="glueparquet", connection_options={"path": "s3://bk-11651651-silver/orders/", "partitionKeys": []}, format_options={"compression": "snappy"}, transformation_ctx="AmazonS3_output")
+AmazonS3_output = glueContext.write_dynamic_frame.from_options(frame=dynamicDF, connection_type="s3", format="glueparquet", connection_options={"path": "s3://bk-silverzone-project1-dev-useast1/sales/", "partitionKeys": []}, format_options={"compression": "snappy"}, transformation_ctx="AmazonS3_output")
 
 job.commit()
