b8d2e9ff2c1326f8080bed7cfa4b649c192bcee6
CarlosGil2001 <carlosyeraldo2001@gmail.com>
2024-04-09 23:26:56 -0500

    update script job

--- jobs/job_silverzone.py
+++ jobs/job_silverzone.py
@@ -4,6 +4,40 @@ from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
+from awsglue.gluetypes import *
+from awsglue import DynamicFrame
+
+def _find_null_fields(ctx, schema, path, output, nullStringSet, nullIntegerSet, frame):
+    if isinstance(schema, StructType):
+        for field in schema:
+            new_path = path + "." if path != "" else path
+            output = _find_null_fields(ctx, field.dataType, new_path + field.name, output, nullStringSet, nullIntegerSet, frame)
+    elif isinstance(schema, ArrayType):
+        if isinstance(schema.elementType, StructType):
+            output = _find_null_fields(ctx, schema.elementType, path, output, nullStringSet, nullIntegerSet, frame)
+    elif isinstance(schema, NullType):
+        output.append(path)
+    else:
+        x, distinct_set = frame.toDF(), set()
+        for i in x.select(path).distinct().collect():
+            distinct_ = i[path.split('.')[-1]]
+            if isinstance(distinct_, list):
+                distinct_set |= set([item.strip() if isinstance(item, str) else item for item in distinct_])
+            elif isinstance(distinct_, str) :
+                distinct_set.add(distinct_.strip())
+            else:
+                distinct_set.add(distinct_)
+        if isinstance(schema, StringType):
+            if distinct_set.issubset(nullStringSet):
+                output.append(path)
+        elif isinstance(schema, IntegerType) or isinstance(schema, LongType) or isinstance(schema, DoubleType):
+            if distinct_set.issubset(nullIntegerSet):
+                output.append(path)
+    return output
+
+def drop_nulls(glueContext, frame, nullStringSet, nullIntegerSet, transformation_ctx) -> DynamicFrame:
+    nullColumns = _find_null_fields(frame.glue_ctx, frame.schema(), "", [], nullStringSet, nullIntegerSet, frame)
+    return DropFields.apply(frame=frame, paths=nullColumns, transformation_ctx=transformation_ctx)
 
 args = getResolvedOptions(sys.argv, ['JOB_NAME'])
 sc = SparkContext()
@@ -12,13 +46,24 @@ spark = glueContext.spark_session
 job = Job(glueContext)
 job.init(args['JOB_NAME'], args)
 
-# Script generated for node AWS Glue Data Catalog
+# Script generated AWS Glue Data Catalog
 AWSGlueDataCatalog = glueContext.create_dynamic_frame.from_catalog(database="db_aws_data", table_name="testbk_1651516_bronze", transformation_ctx="AWSGlueDataCatalog")
 
-# Script generated for node Drop Fields
-DropFields = DropFields.apply(frame=AWSGlueDataCatalog, paths=["orden_electronica", "orden_digitalizada", "documento_estado_ocam", "fecha_formalizacion", "fecha_ultimo_estado", "orden_electronica_generada", "partition_0"], transformation_ctx="DropFields")
+# Script generated Drop Fields
+DropFields = DropFields.apply(frame=AWSGlueDataCatalog, paths=["partition_0", "remote_ratio"], transformation_ctx="DropFields")
+
+# Script generated for node Drop Null Fields
+DropNullFields= drop_nulls(glueContext, frame=DropFields, nullStringSet={"", "null"}, nullIntegerSet={-1}, transformation_ctx="DropNullFields")
+
+# Script generated Rename Field
+RenameField_work_year = RenameField.apply(frame=DropNullFields, old_name="work_year", new_name="year", transformation_ctx="RenameField_work_year")
+RenameField_work_year = RenameField.apply(frame=DropNullFields, old_name="work_year", new_name="year", transformation_ctx="RenameField_work_year")
+RenameField_work_year = RenameField.apply(frame=DropNullFields, old_name="work_year", new_name="year", transformation_ctx="RenameField_work_year")
+RenameField_work_year = RenameField.apply(frame=DropNullFields, old_name="work_year", new_name="year", transformation_ctx="RenameField_work_year")
+RenameField_work_year = RenameField.apply(frame=DropNullFields, old_name="work_year", new_name="year", transformation_ctx="RenameField_work_year")
+RenameField_work_year = RenameField.apply(frame=DropNullFields, old_name="work_year", new_name="year", transformation_ctx="RenameField_work_year")
 
 # Script generated for node Amazon S3
-Write_AmazonS3 = glueContext.write_dynamic_frame.from_options(frame=DropFields, connection_type="s3", format="glueparquet", connection_options={"path": "s3://bk-silverzone-project1-dev-useast2/orders/", "partitionKeys": []}, format_options={"compression": "snappy"}, transformation_ctx="Write_AmazonS3")
+AmazonS3_output = glueContext.write_dynamic_frame.from_options(frame=RenameField_node1712721915302, connection_type="s3", format="glueparquet", connection_options={"path": "s3://bk-11651651-silver/orders/", "partitionKeys": []}, format_options={"compression": "snappy"}, transformation_ctx="AmazonS3_output")
 
 job.commit()
